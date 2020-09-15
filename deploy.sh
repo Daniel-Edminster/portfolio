9a7cf1f124b47a9ec908c8823da3975dd8fbedbd
Daniel Edminster <Daniel.Edminster@outlook.com>
2020-09-14 21:54:26 -0400

    tidied up some loose ends

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
-npm run build
+echo -e "\e[91mBuilding...\e[39m" 
+npm run build 
 cd build
-tar cvf decom.tar *
+echo -e "\e[91mTarballing...\e[39m"
+tar cvf decom.tar * 
+echo -e "\e[91mSending to production server...\e[39m"
 scp decom.tar triumph@danieledminster.com:.
-ssh triumph@danieledminster.com -t "sudo cp ~/decom.tar /var/www/html/. && sudo tar xvf /var/www/html/decom.tar && sudo rm -rf /var/www/html/decom.tar"
+ssh triumph@danieledminster.com -t "sudo cp /home/triumph/decom.tar /var/www/html/. && cd /var/www/html && sudo tar xvf decom.tar && sudo rm -rf /var/www/html/decom.tar"
